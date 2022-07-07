1481424d010e8d1be0ad6574bd4ad9eee7781bb7
Eli Pousson <eli.pousson@gmail.com>
2022-07-06 21:56:26 -0400

    feat: migrate layer_location_data + layer_neatline from overedge

---
+++ R/layer_location_data.R
@@ -0,0 +1,197 @@
+#' Layer location data into a ggplot2 map
+#'
+#' Helper function to make a ggplot2 layer from data returned by
+#' [get_location_data()]. For text geoms, the required aesthetic mapping is
+#' set based on the name_col but values passed to mapping take precedence.
+#'
+#' @details Supported geom function options:
+#'
+#' Options for the geom parameter from the overedge package include:
+#'
+#' - "icon" ([layer_icon]),
+#' - "mapbox" ([layer_mapbox]),
+#' - "markers" ([layer_markers]),
+#' - "numbers" ([layer_numbers])
+#'
+#' Options for the geom parameter from other packages include:
+#'
+#'   - "textsf" ([geomtextpath::geom_textsf])
+#'   - "labelsf" ([geomtextpath::geom_labelsf])
+#'   - "text_repel" ([ggrepel::geom_text_repel])
+#'   - "label_repel" ([ggrepel::geom_label_repel])
+#'   - "mark" ([birdseyeview::layer_show_mark])
+#'   - "location" ([birdseyeview::layer_show_location])
+#'   - "context" ([birdseyeview::layer_show_context])
+#'   - "pattern" ([ggpattern::geom_sf_pattern])
+#'
+#' Alternatively, use the "geom_fn" parameter to pass a function that returns a
+#' ggplot2 layer to use instead of one of the preset geom functions.
+#'
+#' @param geom A character string indicating which ggplot2 geom to use, Default:
+#'   'sf'. Options include "sf" ([ggplot2::geom_sf]), "icon" ([layer_icon]),
+#'   "markers" ([layer_markers]), "text" ([ggplot2::geom_sf_text]), and "label"
+#'   ([ggplot2::geom_sf_label]). See details for a full list.
+#' @param geom_fn ggplot2 geom or custom function using lambda syntax. Use for
+#'   passing custom mapping functions to layer_location_data beyond the
+#'   supported geom options.
+#' @param unit unit to adjust location by dist or diag_ratio; defaults to
+#'   "meter"
+#' @param label_col Column name or id for a column with the text or labels to
+#'   pass to any text geom.
+#' @param ... Parameters passed to selected geom
+#' @inheritParams getdata::get_location_data
+#' @inheritParams ggplot2::geom_sf
+#' @return ggplot2 geom
+#' @rdname layer_location_data
+#' @family layer
+#' @export
+#' @importFrom rlang list2 fn_fmls is_missing exec
+#' @importFrom ggplot2 geom_sf geom_sf_text geom_sf_label
+#' @importFrom purrr discard
+#' @importFrom utils modifyList
+layer_location_data <-
+  function(mapping = NULL,
+           data = NULL,
+           geom = "sf",
+           location = NULL,
+           dist = getOption("maplayer.dist"),
+           diag_ratio = getOption("maplayer.diag_ratio"),
+           unit = getOption("maplayer.unit", default = "meter"),
+           asp = getOption("maplayer.asp"),
+           package = getOption("maplayer.data_package"),
+           filetype = getOption("maplayer.data_filetype"),
+           fn = NULL,
+           geom_fn = NULL,
+           crop = TRUE,
+           trim = FALSE,
+           from_crs = getOption("maplayer.from_crs"),
+           crs = getOption("maplayer.crs"),
+           label_col = "name",
+           ...) {
+    data <-
+      getdata::get_location_data(
+        location = location,
+        dist = dist,
+        diag_ratio = diag_ratio,
+        unit = unit,
+        asp = asp,
+        data = data,
+        url = NULL,
+        path = NULL,
+        package = package,
+        filetype = filetype,
+        fn = fn,
+        crop = crop,
+        trim = trim,
+        from_crs = from_crs,
+        crs = crs,
+        class = "sf",
+        ...
+      )
+
+    params <- rlang::list2(...)
+
+    data <- sfext::as_sf(data)
+
+    if (!is.null(geom_fn)) {
+      return(use_fn(data, geom_fn))
+    }
+
+    overedge_geoms <- c("icon", "mapbox", "markers", "numbers")
+    ggrepel_geoms <- c("text_repel", "label_repel")
+    text_geoms <- c("text", "label", "textsf", "labelsf", ggrepel_geoms)
+    birdseyeview_geoms <- c("mark", "location", "context")
+    ggpattern_geoms <- "pattern"
+
+    # Match geoms
+    geom <-
+      rlang::arg_match(
+        geom,
+        c("sf", overedge_geoms, text_geoms, birdseyeview_geoms, ggpattern_geoms)
+      )
+
+    is_geom_pkg_installed(geom)
+
+    # Assign aesthetics for text/label geoms
+    if (geom %in% text_geoms) {
+      mapping <- modify_mapping(mapping = mapping, label = label_col)
+
+      if (geom %in% ggrepel_geoms) {
+        mapping <- modify_mapping(mapping = mapping, data = data)
+
+        params <- c(params, stat = "sf_coordinates")
+      }
+    }
+
+    geom_chr <- geom
+
+    geom <-
+      switch(geom_chr,
+        "sf" = ggplot2::geom_sf,
+        "icon" = layer_icon,
+        "mapbox" = layer_mapbox,
+        "markers" = layer_markers,
+        "numbers" = layer_numbers,
+        "text" = ggplot2::geom_sf_text,
+        "label" = ggplot2::geom_sf_label,
+        "text_repel" = ggrepel::geom_text_repel,
+        "label_repel" = ggrepel::geom_label_repel,
+        "textsf" = geomtextpath::geom_textsf,
+        "labelsf" = geomtextpath::geom_labelsf,
+        "mark" = layer_marked,
+        "location" = layer_location,
+        "context" = layer_location_context,
+        "pattern" = ggpattern::geom_sf_pattern
+      )
+
+    init_params <- params
+
+    params <-
+      modify_fn_fmls(
+        params = params,
+        fn = geom,
+        mapping = mapping,
+        data = data,
+        keep.null = TRUE
+      )
+
+    # FIXME: This does not seem like the best way of dealing with the default params issue
+    if (any(rlang::has_name(init_params, c("nudge_x", "nudge_y")))) {
+      params$position <- NULL
+    } else {
+      params$nudge_x <- NULL
+      params$nudge_y <- NULL
+    }
+
+    if (geom_chr %in% c("markers", "numbers")) {
+      params$geom <- geom_chr
+    }
+
+    if (!rlang::has_name(init_params, c("direction")) && (geom_chr %in% ggrepel_geoms)) {
+      params$direction <- "both"
+    }
+
+    rlang::exec(geom, !!!params)
+  }
+
+#' Modify function parameters
+#'
+#' @noRd
+#' @importFrom purrr discard
+#' @importFrom utils modifyList
+#' @importFrom rlang fn_fmls list2
+modify_fn_fmls <- function(params, fn, keep_missing = FALSE, keep.null = FALSE, ...) {
+  fmls <- rlang::fn_fmls(fn)
+
+  if (!keep_missing) {
+    fmls <- purrr::discard(fmls, is_missing)
+  }
+
+  params <- c(rlang::list2(...), params)
+
+  utils::modifyList(
+    fmls,
+    params,
+    keep.null = keep.null
+  )
+}
---
+++ R/layer_neatline.R
@@ -0,0 +1,145 @@
+#' Set map limits to a simple feature or bounding box with a specified distance
+#' buffer, aspect ratio, and panel border
+#'
+#' Set limits for a map to the bounding box of an x using
+#' [ggplot2::coord_sf()]. Optionally, adjust the x size by applying a
+#' buffer and/or adjust the aspect ratio of the limiting bounding box to match a
+#' set aspect ratio.
+#'
+#' @inheritParams sfext::st_bbox_ext
+#' @param unit Buffer units; defaults to meter.
+#' @param data `sf` or `bbox` class object
+#' @param crs Coordinate reference system to use for
+#'   [ggplot2::coord_sf()].
+#' @param expand Default `FALSE.` If `TRUE`, the function adds
+#'   [ggplot2::scale_y_continuous()] and
+#'   [ggplot2::scale_x_continuous()] to expand the map extent to
+#'   provided parameters.
+#' @param size Size of panel border, Default: 1
+#' @param color Color of panel border, Default: 'black'
+#' @param bgcolor Fill color of panel background; defaults to "white". If
+#'   "none", panel background is set to ggplot2::element_blank()
+#' @param linetype Line type of panel border, Default: 'solid'
+#' @param hide_grid If `TRUE`, hide major grid lines. Default: `TRUE`
+#' @param label_axes A description of which axes to label passed to
+#'   [ggplot2::coord_sf()]; defaults to '----' which hides axes
+#'   labels.
+#' @param ... Additional parameters to pass to [ggplot2::coord_sf()].
+#' @return [ggplot2::coord_sf()] function with xlim and ylim
+#'   parameters
+#' @example examples/layer_neatline.R
+#' @seealso
+#'  [ggplot2::CoordSf()]
+#' @family layer
+#' @aliases set_map_limits
+#' @export
+#' @importFrom ggplot2 coord_sf scale_y_continuous scale_x_continuous theme element_rect element_blank
+layer_neatline <- function(data = NULL,
+                           dist = getOption("maplayer.dist"),
+                           diag_ratio = getOption("maplayer.diag_ratio"),
+                           unit = getOption("maplayer.unit", default = "meter"),
+                           asp = getOption("maplayer.asp"),
+                           crs = getOption("maplayer.crs"),
+                           color = "black",
+                           bgcolor = "white",
+                           size = 1,
+                           linetype = "solid",
+                           expand = FALSE,
+                           hide_grid = TRUE,
+                           label_axes = "----",
+                           ...) {
+
+  # Pass variables to bbox adjustment function
+  bbox <-
+    sfext::st_bbox_ext(
+    x = data,
+    dist = dist,
+    diag_ratio = diag_ratio,
+    unit = unit,
+    asp = asp,
+    crs = crs
+  )
+
+  # Set limits with adjustments using coord_sf
+  limits <-
+    ggplot2::coord_sf(
+      xlim = c(bbox[["xmin"]], bbox[["xmax"]]),
+      ylim = c(bbox[["ymin"]], bbox[["ymax"]]),
+      label_axes = label_axes,
+      default = TRUE, # Suppress warning about coordinate system being displaced
+      crs = crs,
+      ...
+    )
+
+  if (expand) {
+    expand <-
+      list(
+        ggplot2::scale_y_continuous(expand = c(0, 0)),
+        ggplot2::scale_x_continuous(expand = c(0, 0))
+      )
+  } else {
+    expand <- NULL
+  }
+
+  if (hide_grid) {
+    hide_grid <-
+      list(
+        ggplot2::theme(
+          panel.grid = ggplot2::element_blank()
+        )
+      )
+  } else {
+    hide_grid <- NULL
+  }
+
+  if (label_axes == "----") {
+    label_axes <-
+      ggplot2::theme(
+        axis.title = ggplot2::element_blank(),
+        axis.text = ggplot2::element_blank(),
+        axis.ticks = ggplot2::element_blank(),
+        axis.ticks.length = ggplot2::unit(x = 0, units = "mm"),
+        axis.line = ggplot2::element_blank()
+      )
+  } else {
+    label_axes <- NULL
+  }
+
+  if (is.na(bgcolor) || bgcolor == "none") {
+    panel_background <-
+      ggplot2::element_blank()
+
+    plot_background <-
+      ggplot2::element_blank()
+  } else {
+    panel_background <-
+      ggplot2::element_rect(fill = bgcolor)
+
+    plot_background <-
+      ggplot2::element_rect(fill = bgcolor)
+  }
+
+  if (is.na(color) || color == "none") {
+    panel_border <-
+      ggplot2::element_blank()
+  } else {
+    panel_border <-
+      ggplot2::element_rect(color = color, size = size, linetype = linetype, fill = NA)
+  }
+
+  panel_theme <-
+    ggplot2::theme(
+      panel.border = panel_border,
+      panel.background = panel_background,
+      plot.background = plot_background
+    )
+
+
+  list(
+    limits,
+    expand,
+    hide_grid,
+    label_axes,
+    panel_theme
+  )
+}
