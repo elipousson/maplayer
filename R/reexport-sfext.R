5deff959ced38ab7a2a44c8af3c943ae13bf9674
Eli Pousson <eli.pousson@gmail.com>
2022-07-06 21:57:48 -0400

    refactor: switch overedge reexport for sfext reexport

---
+++ R/reexport-sfext.R
@@ -0,0 +1,43 @@
+#' @importFrom sfext st_buffer_ext
+#' @export
+sfext::st_buffer_ext
+
+#' @importFrom sfext st_bbox_ext
+#' @export
+sfext::st_bbox_ext
+
+#' @importFrom sfext st_transform_ext
+#' @export
+sfext::st_transform_ext
+
+#' @importFrom sfext as_sf
+#' @export
+sfext::as_sf
+
+#' @importFrom sfext as_sf_list
+#' @export
+sfext::as_sf_list
+
+#' @importFrom sfext is_sf
+#' @export
+sfext::is_sf
+
+#' @importFrom sfext is_sf_list
+#' @export
+sfext::is_sf_list
+
+#' @importFrom sfext is_point
+#' @export
+sfext::is_point
+
+#' @importFrom sfext is_multipoint
+#' @export
+sfext::is_multipoint
+
+#' @importFrom sfext is_polygon
+#' @export
+sfext::is_polygon
+
+#' @importFrom sfext is_multipolygon
+#' @export
+sfext::is_multipolygon
