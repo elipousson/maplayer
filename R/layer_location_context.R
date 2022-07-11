1533dd28a23f1ce16c03afe5326cc076f5c0e237
Eli Pousson <eli.pousson@gmail.com>
2022-07-11 18:14:54 -0400

    refactor: add location parameter for consistency w/ other location functions
    - refactor: add location parameter for consistency w/ other location functions
    - refactor: don't add theme_void to neatline_layer

--- R/layer_location_context.R
+++ R/layer_location_context.R
@@ -2,18 +2,20 @@
 #'
 #' Intended for use with inset_map function.
 #'
-#' @param data sf object with location data
+#' @param data,location data can be a sf object for the location or a ggplot
+#'   layer representing the location. location can only be an sf object (not a
+#'   ggplot2 layer) and it is used instead of data if both are provided.
 #' @param fill Fill color for location data
 #' @param color Edge color for location data
-#' @param context sf or bbox object for context area or a `geom` layer
+#' @param context sf or bbox object for context area or ggplot layer
+#'   representing the context.
 #' @param context_aes list with aesthetic attributes for context area; must
 #'   include fill and color; defaults to list(fill = "white", color = "black",
 #'   ...)
 #' @param layer_between An optional ggplot2 layer to insert between the context
 #'   layer and the location layer.
 #' @param neatline If `TRUE`, add a neatline layer to the returned layer
-#' @param ... Additional parameters passed to layer_location_data for
-#'   location
+#' @param ... Additional parameters passed to layer_location_data for location
 #' @inheritParams layer_location_data
 #' @name layer_location_context
 #' @aliases layer_show_context
@@ -22,6 +24,7 @@
 #' @importFrom sf st_crs
 #' @importFrom ggplot2 theme_void
 layer_location_context <- function(data = NULL,
+                                   location = NULL,
                                    fill = "gray70",
                                    color = "black",
                                    context = NULL,
@@ -30,41 +33,35 @@ layer_location_context <- function(data = NULL,
                                    crs = NULL,
                                    neatline = TRUE,
                                    ...) {
-  if (sfext::is_sf(data)) {
-    # FIXME: Is this crs check actually required?
-    if (is.null(crs)) {
-      crs <- sf::st_crs(data)
+
+  if ("gg" %in% class(data)) {
+    location_layer <- data
+  } else if (sfext::is_sf(data) | sfext::is_sf(location)) {
+    if (!is.null(location)) {
+      data <- location
     }
 
     location_layer <-
       layer_location_data(data = data, fill = fill, color = color, crs = crs, ...)
-  } else if ("gg" %in% class(data)) {
-    location_layer <- data
   }
 
-  is_context <- sfext::is_sf(context)
-
-  if (is_context) {
+  if ("gg" %in% class(context)) {
+    context_layer <- context
+  } else if (sfext::is_sf(context)) {
     context_layer <-
       layer_location_data(data = context, fill = context_aes$fill, color = context_aes$color, alpha = context_aes$alpha, crs = crs)
-  } else if ("gg" %in% class(context)) {
-    context_layer <- context
   }
 
   neatline_layer <- NULL
 
   if (neatline && is_context) {
     neatline_layer <-
-      list(
         layer_neatline(
           data = context,
           color = NA,
           bgcolor = "none",
           crs = crs
-        ),
-        # FIXME: Adding theme_void here is heavy-handed
-        ggplot2::theme_void()
-      )
+        )
   }
 
   list(
