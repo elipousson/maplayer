26dc02ad6d91281055bdfd51cd13e969f741058b
Eli Pousson <eli.pousson@gmail.com>
2022-07-11 16:26:02 -0400

    refactor: remove sfext functions + add ggplot to reexports

--- R/reexport.R
+++ R/reexport.R
@@ -2,46 +2,6 @@
 #' @export
 ggplot2::aes
 
-#' @importFrom sfext st_buffer_ext
+#' @importFrom ggplot2 last_plot
 #' @export
-sfext::st_buffer_ext
-
-#' @importFrom sfext st_bbox_ext
-#' @export
-sfext::st_bbox_ext
-
-#' @importFrom sfext st_transform_ext
-#' @export
-sfext::st_transform_ext
-
-#' @importFrom sfext as_sf
-#' @export
-sfext::as_sf
-
-#' @importFrom sfext as_sf_list
-#' @export
-sfext::as_sf_list
-
-#' @importFrom sfext is_sf
-#' @export
-sfext::is_sf
-
-#' @importFrom sfext is_sf_list
-#' @export
-sfext::is_sf_list
-
-#' @importFrom sfext is_point
-#' @export
-sfext::is_point
-
-#' @importFrom sfext is_multipoint
-#' @export
-sfext::is_multipoint
-
-#' @importFrom sfext is_polygon
-#' @export
-sfext::is_polygon
-
-#' @importFrom sfext is_multipolygon
-#' @export
-sfext::is_multipolygon
+ggplot2::last_plot
