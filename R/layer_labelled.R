609861bd0a561fbcea22acaa0459a9b93a6375bb
Eli Pousson <eli.pousson@gmail.com>
2022-07-13 14:45:03 -0400

    refactor: improve input checking for layer_labelled

--- R/layer_labelled.R
+++ R/layer_labelled.R
@@ -1,30 +1,33 @@
 #' Label simple feature objects in a location
 #'
 #' Label markers, streets, areas, or other simple feature objects using any of
-#' the following geoms: "text", "label", "textsf", "labelsf", "text_repel", or
-#' "label_repel".
+#' the following geoms: "text", "sf_text", "label", "sf_label", "textsf",
+#' "labelsf", "text_repel", or "label_repel".
 #'
-#' Note:
-#'
-#' - Unlike in some overedge package functions, fn is applied to the whole data; not a
-#' subset of the data based on location.
-#' - For this function, dist and unit are both used by [sfext::st_clip()] (not by
-#' [layer_location_data])
+#' Note: Unlike in some [getdata] package functions, fn is applied to all of the
+#' data; not a subset of the data based on location. For this function, dist and
+#' unit are both used by [sfext::st_clip()] (not by [layer_location_data])
 #'
 #' The function also overrides the label aesthetics to hide the colored letters
 #' that would otherwise appear when using a theme legend.
 #'
-#' @param data Data to use for label creation
-#' @param location Location to label (if not specified the data is assumed to conver the whole location);
-#' @param fn Function to apply to data before creating labels; can be used in the creation of the label_col
+#' @param data Data to use for labels (must be an sf object or a data frame that
+#'   can be converted to an sf object using [sfext::as_sf])
+#' @param location Location to label (if not specified the data is assumed to
+#'   conver the whole location);
+#' @param fn Function to apply to data before creating labels; can be used in
+#'   the creation of the label_col
 #' @param label_col Label column name
-#' @param geom A geom to use "text", "label", "textsf", "labelsf", "text_repel", or "label_repel"
+#' @param geom A geom to use "text", "label", "textsf", "labelsf", "text_repel",
+#'   or "label_repel"
 #' @param mapping Aesthetic mapping, Default: NULL
 #' @param union If TRUE, group by label_col and union geometry, Default: `FALSE`
-#' @param drop_shadow If `TRUE`, use [ggfx::with_shadow] to add a drop shadow to the label layer. Defaults to `FALSE`.
-#' @param x_offset,y_offset,sigma Parameters passed to ggfx::with_shadow if `drop_shadow = TRUE`.
+#' @param drop_shadow If `TRUE`, use [ggfx::with_shadow] to add a drop shadow to
+#'   the label layer. Defaults to `FALSE`.
+#' @param x_offset,y_offset,sigma Parameters passed to [ggfx::with_shadow] if
+#'   `drop_shadow = TRUE`.
 #' @inheritParams sfext::st_clip
-#' @param ... Additional parameters passed to [layer_location_data]
+#' @inheritDotParams layer_location_data -geom_fn -from_crs
 #' @seealso
 #'  [sfext::st_clip()],[layer_location_data()]
 #' @rdname layer_labelled
@@ -32,7 +35,7 @@
 #' @export
 #' @importFrom dplyr summarise
 #' @importFrom sf st_union
-#' @importFrom sfext st_clip
+#' @importFrom sfext check_sf st_clip
 #' @importFrom ggplot2 guides guide_legend aes
 layer_labelled <- function(data,
                            location = NULL,
@@ -43,44 +46,60 @@ layer_labelled <- function(data,
                            union = FALSE,
                            clip = NULL,
                            dist = NULL,
+                           diag_ratio = NULL,
                            unit = NULL,
                            drop_shadow = FALSE,
                            x_offset = 5,
                            y_offset = 5,
                            sigma = 0.5,
                            ...) {
+  if (!is_sf(data) && is.data.frame(data)) {
+    # FIXME: If data is a dataframe, there should be a way of passing from_crs,
+    # coords_col or other relevant parameter for conversion. Otherwise this only
+    # works with data frames that match the default expectations.
+    data <- as_sf(data)
+  }
+
   if (!is.null(fn)) {
     fn <- as_function(fn)
     data <- fn(data)
   }
 
+  sfext::check_sf(data)
+
   if (union) {
+    # FIXME: This pattern should be added as a feature to sfext::st_union_ext
     data <- group_by_col(data, col = label_col)
     data <- dplyr::summarise(data, geometry = sf::st_union(geometry))
   }
 
-  if (is.character(clip)) {
-    # FIXME: st_clip likely should be passed to fn for layer_location_data to apply clip to any subset of the data specified by location rather than the whole area
-    clip_fn <- as_function(~ sfext::st_clip(x = .x, clip = clip, dist = dist))
-  } else if (!is.null(clip)) {
-    clip_fn <- as_function(~ sfext::st_clip(x = data, clip = clip, dist = dist))
-  } else {
-    clip_fn <- NULL
+  clip_fn <- NULL
+
+  if (!is.null(clip)) {
+    # FIXME: st_clip likely should be passed to fn for layer_location_data to
+    # apply clip to any subset of the data specified by location rather than the
+    # whole area
+    clip_fn <- as_function(~ sfext::st_clip(x = .x, clip = clip, dist = dist, diag_ratio = diag_ratio, unit = unit))
   }
 
-  # TODO: Double-check this works
-  if (geom %in% c("text", "label", "textsf", "labelsf", "text_repel", "label_repel")) {
-    label_layer <-
-      layer_location_data(
-        location = location,
-        fn = clip_fn,
-        data = data,
-        mapping = mapping,
-        geom = geom,
-        label_col = label_col,
-        ...
-      )
-  }
+  # TODO: Duplicate with layer_location_data - consider exporting as internal reference data
+  ggplot_text_geoms <- c("text", "sf_text", "label", "sf_label")
+  ggrepel_geoms <- c("text_repel", "label_repel")
+  geomtextpath_geoms <- c("textsf", "labelsf")
+  text_geoms <- c(ggplot_text_geoms, geomtextpath_geoms, ggrepel_geoms)
+
+  geom <- arg_match(geom, text_geoms)
+
+  label_layer <-
+    layer_location_data(
+      data = data,
+      location = location,
+      fn = clip_fn,
+      mapping = mapping,
+      geom = geom,
+      label_col = label_col,
+      ...
+    )
 
   if (drop_shadow) {
     label_layer <-
@@ -92,14 +111,12 @@ layer_labelled <- function(data,
       )
   }
 
-
-  label_layer <-
-    list(
-      label_layer,
-      ggplot2::guides(
-        fill = ggplot2::guide_legend(
-          override.aes = ggplot2::aes(label = "")
-        )
+  list(
+    label_layer,
+    ggplot2::guides(
+      fill = ggplot2::guide_legend(
+        override.aes = ggplot2::aes(label = "")
       )
     )
+  )
 }
