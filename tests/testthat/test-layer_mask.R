6078cdb6d4d8901388ff841005da84edec9b22e5
Eli Pousson <eli.pousson@gmail.com>
2022-07-14 14:59:46 -0400

    test: avoid loading ggplot2 + add skip_on_ci

--- tests/testthat/test-layer_mask.R
+++ tests/testthat/test-layer_mask.R
@@ -1,24 +1,22 @@
 test_that("layer_mask works", {
-  library(ggplot2)
   nc <- sf::st_read(system.file("shape/nc.shp", package = "sf"))
 
+  skip_on_ci()
   expect_s3_class(
-    ggplot() +
-      geom_sf(data = nc) +
+    ggplot2::ggplot() +
+      ggplot2::geom_sf(data = nc) +
       layer_mask(data = nc[1, ], mask = nc),
     "gg"
   )
-
   expect_s3_class(
-    ggplot() +
-      geom_sf(data = nc) +
+    ggplot2::ggplot() +
+      ggplot2::geom_sf(data = nc) +
       layer_mask(data = nc[10, ], diag_ratio = 3, neatline = TRUE),
     "gg"
   )
-
   expect_s3_class(
-    ggplot() +
-      geom_sf(data = nc) +
+    ggplot2::ggplot() +
+      ggplot2::geom_sf(data = nc) +
       layer_mask(data = nc[10, ], dist = 100000, neatline = TRUE),
     "gg"
   )
