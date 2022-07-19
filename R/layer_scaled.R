0553095ccf940bdfaf550379a59a5e5c6be9344a
Eli Pousson <eli.pousson@gmail.com>
2022-07-18 21:55:59 -0400

    refactor: minor improvements to layer_scaled

--- R/layer_scaled.R
+++ R/layer_scaled.R
@@ -1,6 +1,7 @@
 #' Create a ggplot2 layer scaled to a paper and orientation for a location
 #'
-#' Uses [layer_neatline], [sfext::standard_scales], and [sfext::convert_dist_scale].
+#' Uses [layer_neatline], [sfext::standard_scales], and
+#' [sfext::convert_dist_scale].
 #'
 #' @inheritParams sfext::convert_dist_scale
 #' @inheritParams sfext::st_bbox_ext
@@ -10,13 +11,14 @@
 #' @name layer_scaled
 #' @export
 #' @importFrom sfext convert_dist_scale sf_bbox_ydist sf_bbox_xdist
+#' @importFrom sf st_centroid
 layer_scaled <-
   function(data = NULL,
            dist = NULL,
            diag_ratio = NULL,
            unit = NULL,
            asp = NULL,
-           crs = NULL,
+           crs = getOption("maplayer.crs", default = 3857),
            scale = NULL,
            paper = NULL,
            orientation = NULL,
@@ -30,9 +32,12 @@ layer_scaled <-
         scale = scale
       )
 
-    stopifnot(
-      nrow(scaled_paper) == 1
-    )
+    if (nrow(scaled_paper) > 1) {
+      cli_abort(c(
+        "{.arg paper}, {.arg orientation}, and {.arg scale} parameters returned multiple options.",
+        "More specific parameters are required."
+      ))
+    }
 
     if (is.null(asp)) {
       asp <- scaled_paper$asp
@@ -40,10 +45,11 @@ layer_scaled <-
 
     if (!is_sf(data, ext = TRUE)) {
       cli_abort("data must be a bounding box or simple feature object.")
-    } else {
-      # Get adjusted bounding box for data
-      bbox <-
-        sfext::st_bbox_ext(
+    }
+
+    # Get adjusted bounding box for data
+    bbox <-
+      sfext::st_bbox_ext(
         x = data,
         dist = dist,
         diag_ratio = diag_ratio,
@@ -51,9 +57,9 @@ layer_scaled <-
         asp = asp,
         crs = crs
       )
-    }
 
     # Compare bbox xdist and ydist to actual dimensions
+    # FIXME: move this into a helper function
     ydist <- sfext::sf_bbox_ydist(bbox, units = TRUE)
     xdist <- sfext::sf_bbox_xdist(bbox, units = TRUE)
 
@@ -64,19 +70,18 @@ layer_scaled <-
 
     if (!clip && !fit_check) {
       cli_abort("This data covers a larger area than can be displayed at this scale ({scale}) on this paper ({paper})")
-    } else {
-      center <- sfext::st_center(data, ext = TRUE)
-      scaled_dist <- max(c(scaled_paper$width_actual, scaled_paper$height_actual)) / 2
-
-      scaled_bbox <-
-        sfext::st_bbox_ext(
-          x = center$sf,
-          dist = scaled_dist,
-          asp = scaled_paper$asp,
-          unit = scaled_paper$unit_actual,
-          crs = crs
-        )
     }
+
+    scaled_dist <- max(c(scaled_paper$width_actual, scaled_paper$height_actual)) / 2
+
+    scaled_bbox <-
+      sfext::st_bbox_ext(
+        x = sf::st_centroid(data),
+        dist = scaled_dist,
+        asp = scaled_paper$asp,
+        unit = scaled_paper$unit_actual,
+        crs = crs
+      )
 
     scaled_layer <-
       layer_neatline(
