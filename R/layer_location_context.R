72f9acff42c8a8f55fc83800836614da0d4578fa
Eli Pousson <eli.pousson@gmail.com>
2022-08-11 00:36:26 -0700

    refactor: rename context_aes to context_params
    - refactor: rename context_aes to context_params
    - feat: allow formula/function location values to subset context data

--- R/layer_location_context.R
+++ R/layer_location_context.R
@@ -1,60 +1,69 @@
-#' Create a map layer showing a location in context of another area or location
+#' Create a layer showing a location and related context
 #'
-#' Intended for use with inset_map function.
+#' Intended for use with [make_inset_map] function.
 #'
-#' @param data,location data can be a sf object for the location or a ggplot
-#'   layer representing the location. location can only be an sf object (not a
-#'   ggplot2 layer) and it is used instead of data if both are provided.
-#' @param fill Fill color for location data
-#' @param color Edge color for location data
-#' @param context sf or bbox object for context area or ggplot layer
-#'   representing the context.
-#' @param context_aes list with aesthetic attributes for context area; must
-#'   include fill and color; defaults to list(fill = "white", color = "black",
-#'   ...)
+#' @param data,location data and location can be a either sf object for the
+#'   location or a ggplot layer representing the location. location can also be
+#'   a formula or a function used to subset context (requires context to be an
+#'   sf object). If data and context are both provided and both are sf objects,
+#'   data is ignored.
+#' @param fill,color Fill and color aesthetics for location data
+#' @param context A sf object for context area or a ggplot layer representing
+#'   the context.
+#' @param context_params list with parameters for context layer; defaults to
+#'   list(fill = "white", color = "black", alpha = 1, ...).
 #' @param layer_between An optional ggplot2 layer to insert between the context
 #'   layer and the location layer.
-#' @param neatline If `TRUE`, add a neatline layer to the returned layer
-#' @param ... Additional parameters passed to layer_location_data for location
+#' @param neatline If `TRUE`, add a neatline layer to the returned layer.
+#'   Requires that data be an sf object and not inherited.
+#' @param ... Additional parameters passed to [layer_location_data] for location
 #' @inheritParams layer_location_data
 #' @name layer_location_context
 #' @aliases layer_show_context
 #' @export
 #' @importFrom sfext is_sf
-#' @importFrom sf st_crs
-#' @importFrom ggplot2 theme_void
 layer_location_context <- function(data = NULL,
                                    location = NULL,
                                    fill = "gray70",
                                    color = "black",
                                    context = NULL,
-                                   context_aes = list(fill = "white", color = "black", alpha = 1, ...),
+                                   context_params = list(fill = "white", color = "black", alpha = 1, ...),
                                    layer_between = NULL,
                                    crs = getOption("maplayer.crs", default = 3857),
                                    neatline = TRUE,
                                    basemap = FALSE,
                                    ...) {
-  if ("gg" %in% class(data)) {
-    location_layer <- data
-  } else if (sfext::is_sf(data) | sfext::is_sf(location)) {
+  sf_context <- sfext::is_sf(context)
+
+  if (is_function(location) && sf_context) {
+    data <- location(context)
+  } else if (is_formula(location) && sf_context) {
+    data <- use_fn(context, location)
+  }
+
+  if (sfext::is_sf(data) | sfext::is_sf(location)) {
     if (!is.null(location)) {
       data <- location
     }
 
     location_layer <-
       layer_location_data(data = data, fill = fill, color = color, crs = crs, ...)
+  } else if ("gg" %in% class(data)) {
+    location_layer <- data
+  } else if ("gg" %in% class(location)) {
+    location_layer <- location
   }
 
-  if ("gg" %in% class(context)) {
-    context_layer <- context
-  } else if (sfext::is_sf(context)) {
+  if (sf_context) {
     context_layer <-
-      layer_location_data(data = context, fill = context_aes$fill, color = context_aes$color, alpha = context_aes$alpha, crs = crs)
+      eval_tidy(quo(layer_location_data(data = context, !!!context_params)))
+  } else if ("gg" %in% class(context)) {
+    context_layer <- context
   }
 
   neatline_layer <- NULL
 
-  if (neatline && is_sf(context)) {
+  if (neatline && sf_context) {
     neatline_layer <-
       layer_neatline(
         data = context,
@@ -73,5 +82,4 @@ layer_location_context <- function(data = NULL,
     )
 
   make_basemap(layer_stack, basemap)
-
 }
