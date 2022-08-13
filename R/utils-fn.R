711670058ee149de451f104750b5b70b9d7bf710
Eli Pousson <eli.pousson@gmail.com>
2022-08-13 11:26:27 -0700

    feat: add eval_tidy_fn utility function
    - feat: add eval_tidy_fn utility function (supports with_smooth and with_shadow)
    - refactor: split make_fn off from use_fn
    - feat: add is_fn to check for formulas and functions

---
+++ R/utils-fn.R
@@ -0,0 +1,88 @@
+#' Is this object a function or a formula?
+#'
+#' If x is a function or a formula, return `TRUE`.
+#'
+#' @param x A object to check.
+#' @noRd
+#' @importFrom rlang is_function is_formula
+is_fn <- function(x) {
+  rlang::is_function(x) | rlang::is_formula(x)
+}
+
+#' Make a function from a formula
+#'
+#' @param fn Either a formula or function.
+#'
+#' If a __function__, it is used as is.
+#'
+#' If a __formula__, e.g. `~ .x + 2`, it is converted to a function.
+#'
+#' @inheritParams cli::cli_abort
+#' @noRd
+#' @importFrom rlang is_function is_formula as_function
+make_fn <- function(fn, ..., arg = caller_arg(x), call = caller_env()) {
+  if (rlang::is_function(fn)) {
+    return(fn)
+  }
+
+  if (!rlang::is_formula(fn)) {
+    cli_abort(
+      c("{.arg {arg}} must be a formula or function.",
+        "i" = "You've supplied a {class(fn)} object."
+      ),
+      arg = arg,
+      call = call
+    )
+  }
+
+  rlang::as_function(fn)
+}
+
+#' Use a function or formula on an object x
+#'
+#' @param x Object to pass as first argument of function.
+#' @inheritParams make_fn
+#' @param ... Additional arguments to pass to function.
+#' @noRd
+use_fn <- function(x = NULL, fn = NULL, ..., arg = caller_arg(fn), call = caller_env()) {
+  if (is.null(fn)) {
+    return(x)
+  }
+
+  fn <- make_fn(fn, arg = arg, call = call)
+
+  fn(x, ...)
+}
+
+#' Evaluate an function with spliced parameters
+#'
+#' @param x Object to pass as first argument of function.
+#' @param param Parameters to splice as additional arguments of function.
+#' @param pkg Package name passed to [is_pkg_installed]
+#' @noRd
+#' @importFrom rlang eval_tidy quo is_logical
+eval_tidy_fn <- function(x, params = NULL, pkg = NULL, fn = NULL, arg = caller_arg(fn), call = caller_env()) {
+  if (is.null(params)) {
+    return(x)
+  }
+
+  is_pkg_installed(pkg)
+
+  fn <- make_fn(fn, arg = arg, call = call)
+
+  if (is.list(params)) {
+    rlang::eval_tidy(rlang::quo(fn(x, !!!params)))
+  } else if (rlang::is_logical(params) && params) {
+    fn(x)
+  }
+}
+
+#' @noRd
+with_smooth <- function(x, params = NULL) {
+  eval_tidy_fn(x, params, "smoothr", smoothr::smooth)
+}
+
+#' @noRd
+with_shadow <- function(x, params = NULL) {
+  eval_tidy_fn(x, params, "ggfx", ggfx::with_shadow)
+}
--- R/utils.R
+++ R/utils.R
@@ -14,19 +14,6 @@ utils::globalVariables(
   )
 )
 
-#' Create basemap by adding the object (layer or layer list) to [ggplot2::ggplot()]
-#' @noRd
-#' @importFrom ggplot2 ggplot
-make_basemap <- function(x, basemap = FALSE) {
-  if (!basemap) {
-    return(x)
-  }
-
-  ggplot2::ggplot() +
-    x
-}
-
-
 #' Group data by column if present
 #'
 #' @param data Data frame or simple feature object
@@ -119,38 +106,27 @@ modify_mapping <- function(mapping = NULL, data = NULL, ...) {
   return(mapping)
 }
 
-#' Apply function to data
-#'
-#' @param data Data to apply function to
-#' @param fn defaults to NULL
-#' @noRd
-use_fn <- function(data, fn = NULL) {
-  if (is.null(fn)) {
-    return(data)
-  }
-
-  fn <- as_function(fn)
-  fn(data)
-}
-
 #' Does the data frame has a column with the same name?
 #'
 #' @noRd
 #' @importFrom dplyr rename
+#' @importFrom rlang has_name
 has_same_name_col <- function(x, col = NULL, prefix = "orig", ask = FALSE, quiet = FALSE) {
-  if (has_name(x, col)) {
+  if (rlang::has_name(x, col)) {
     new_col <- paste0(prefix, "_", col)
 
     if (ask && !quiet) {
-      if (!cli_yeah("The provided data includes an existing column named '{col}'.
-                   Do you want to proceed and rename this column to {new_col}?")) {
+      if (!cli_yeah(
+      "The provided data includes an existing column named '{col}'.
+      Do you want to proceed and rename this column to {new_col}?")) {
         cli_abort("Please rename your column to use this function.")
       }
     }
 
     if (!quiet) {
       cli_inform(
-        c("v" = "The existing column '{col}' to '{new_col}' to avoid overwriting any existing values.")
+        c("v" = "The existing column '{col}' to '{new_col}' to avoid
+          overwriting any existing values.")
       )
     }
 
@@ -166,21 +142,20 @@ has_same_name_col <- function(x, col = NULL, prefix = "orig", ask = FALSE, quiet
 
 #' Is this package installed?
 #'
-#' @param package Name of a package.
-#' @param repo GitHub repository to use for the package.
+#' @param package Package name.
+#' @param repo Package repository passed to pkg parameter of
+#'   [rlang::check_installed].
 #' @noRd
-is_pkg_installed <- function(pkg, repo = NULL) {
-  if (!is_installed(pkg = pkg)) {
-    if (!is.null(repo)) {
-      pkg <- repo
-    }
-
-    check_installed(pkg = pkg)
+#' @importFrom rlang is_installed check_installed
+is_pkg_installed <- function(pkg = NULL, repo = NULL) {
+  if (!is.null(pkg) && !rlang::is_installed(pkg = pkg)) {
+    rlang::check_installed(pkg = repo %||% pkg)
   }
 }
 
-#' Is the package needed for this geom installed?
+#' Is the package needed for these selected geom functions installed?
 #'
+#' @param geom geom function name as character.
 #' @noRd
 is_geom_pkg_installed <- function(geom) {
   ggpattern_geoms <- c("pattern", "sf_pattern")
