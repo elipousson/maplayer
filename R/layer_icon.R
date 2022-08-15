e0d41ab5e3bbfe18d4bf8c443012c463f3fb0f9d
Eli Pousson <eli.pousson@gmail.com>
2022-08-14 17:17:52 -0700

    feat: get layer_icon working w/ inherited data
    - feat: get layer_icon working w/ inherited data (may still break if sf_column is not named "geometry")
    - refactor: reorganize function to work w/ join_map_icons and get_map_icon helper functions

--- R/layer_icon.R
+++ R/layer_icon.R
@@ -1,6 +1,6 @@
-#' Add a SVG icons to a ggplot2 plot based on a simple feature object
+#' Use ggsvg to add a layer with icons at feature locations
 #'
-#' Use the  [ggsvg::geom_point_svg()] function to plot icons using the centroids
+#' Use the [ggsvg::geom_point_svg()] function to plot icons using the centroids
 #' from the input simple feature object to set the icon location.
 #'
 #' @param data A `sf` object. Any objects with polygon geometry are converted to
@@ -12,18 +12,18 @@
 #'   value. Note that the icon column should not be mapped with
 #'   [ggplot2::aes()].
 #' @param icon Icon name. Default `NULL`. If `icon` is provided, `iconname_col`
-#'   is not used.
+#'   is ignored. See `map_icons$name` for supported options.
 #' @param px Icon size in pixels. See `map_icons$px` for supported options.
 #'   Optional but may be necessary to differentiate icons with duplicate names.
 #' @param source Icon source. See `map_icons$repo` for supported options.
 #'   Optional but may be required to differentiate icons with duplicate names.
 #' @param svg Optional. Custom file path or URL with SVG to pass to `svg`
 #'   parameter for  [ggsvg::geom_point_svg()].  If `icon` is provided, `svg` is
-#'   not used.
-#' @param color SVG color passed to  [ggsvg::geom_point_svg()]. default color is
-#'   set to "black".
+#'   ignored.
+#' @param color Icon color passed to [ggsvg::geom_point_svg()]; defaults to
+#'   "black".
 #' @param crs Coordinate reference system; defaults to `NULL`.
-#' @param ... Additional parameters to  [ggsvg::geom_point_svg()].
+#' @inheritDotParams ggsvg::geom_point_svg
 #' @example examples/layer_icon.R
 #' @seealso
 #'  [ggsvg::geom_point_svg()]
@@ -44,111 +44,175 @@ layer_icon <- function(data = NULL,
                        color = "black",
                        crs = getOption("maplayer.crs", default = 3857),
                        ...) {
-  is_pkg_installed(pkg = "ggsvg", repo = "coolbutuseless/ggsvg")
+  is_pkg_installed(pkg = "ggsvg")
 
-  if (is_sf(data) && !sfext::is_point(data)) {
-    data <- suppressWarnings(sf::st_centroid(data))
-  }
-
-  sfext::check_sf(data)
-
-  data <- sfext::st_transform_ext(data, crs = crs)
-
-  if ((iconname_col %in% names(data)) & is.null(icon)) {
-    icon_options <-
-      dplyr::rename(
-        map_icons,
-        svg_url = url,
-        {{ iconname_col }} := name
-      )
-
-    if (!is.null(px)) {
-      icon_options <-
-        dplyr::filter(
-          icon_options,
-          .data$px == px
-        )
-    }
-
-    if (!is.null(source)) {
-      icon_options <-
-        dplyr::filter(
-          icon_options,
-          .data$repo == source
-        )
-    }
-
+  if (!is.null(iconname_col) && is.null(icon)) {
     data <-
-      dplyr::left_join(
-        data,
-        icon_options,
-        by = {{ iconname_col }}
-      )
-
-    data <-
-      dplyr::mutate(
-        dplyr::rowwise(data),
-        svg_url = paste(readLines(svg_url), collapse = "\n")
-      )
-
-    icon_layer <-
-      ggsvg::geom_point_svg(
+      join_map_icons(
         data = data,
-        ggplot2::aes(
-          geometry = .data[[attributes(data)$sf_column]],
-          svg = .data[["svg_url"]]
+        iconname_col = iconname_col,
+        px = px,
+        source = source,
+        single = FALSE,
+        crs = crs
+      )
+
+    ggsvg::geom_point_svg(
+      mapping = ggplot2::aes(
+        geometry = .data[[attributes(data)$sf_column %||% "geometry"]],
+        svg = .data[["svg"]]
+      ),
+      data = data,
+      color = color,
+      stat = "sf_coordinates",
+      ...
+    )
+  } else {
+    if (!is.null(icon)) {
+      svg <-
+        get_map_icon(
+          icon = icon,
+          source = source,
+          px = px,
+          single = TRUE
+        )[["svg"]]
+    }
+
+    if (is.null(svg)) {
+      cli::cli_abort(
+        "{.arg iconname_col}, {.arg icon}, or {.arg svg} must be provided."
+      )
+    }
+
+    suppressWarnings(
+      ggsvg::geom_point_svg(
+        mapping = ggplot2::aes(
+          geometry = .data[[attributes(data)$sf_column %||% "geometry"]]
         ),
+        data = data,
+        svg = svg,
+        color = color,
         stat = "sf_coordinates",
         ...
       )
-  } else if (!is.null(icon)) {
-    icon <- dplyr::filter(map_icons, .data$name == icon)
-
-    if (!is.null(px)) {
-      icon <- dplyr::filter(icon, .data$size == px)
-    }
-
-    if (!is.null(source)) {
-      icon <- dplyr::filter(icon, stringr::str_detect(repo, source))
-    }
-
-    if (nrow(icon) > 1) {
-      cli_abort(
-        c("The provided parameters match more than one icon.",
-          "i" = "Provide the `px` and/or `source` to select a single icon."
-        )
-      )
-    }
-
-    icon_layer <-
-      suppressWarnings(
-        ggsvg::geom_point_svg(
-          data = data,
-          ggplot2::aes(geometry = .data[[attributes(data)$sf_column]]),
-          svg = paste(readLines(icon$url), collapse = "\n"),
-          stat = "sf_coordinates",
-          color = color,
-          defaults = list(color = "black"),
-          ...
-        )
-      )
-  } else if (!is.null(svg)) {
-    icon_layer <-
-      suppressWarnings(
-        ggsvg::geom_point_svg(
-          data = data,
-          ggplot2::aes(x = lon, y = lat),
-          svg = svg,
-          color = color,
-          ...
-        )
-      )
+    )
   }
-
-  icon_layer
 }
 
 #' @rdname layer_icon
 #' @name geom_sf_icon
 #' @export
 geom_sf_icon <- layer_icon
+
+#' Get subset of icons from map_icons data
+#'
+#' @param single If `TRUE` and parameters match more than one icon, return an
+#'   error. If `FALSE`, return a tibble with all matching icons..
+#' @param read If TRUE, read url from `map_icons` to text for SVG using
+#'   [readLines()]. Defaults to `TRUE`.
+#' @noRd
+#' @importFrom dplyr filter bind_cols
+#' @importFrom purrr map_chr
+get_map_icon <- function(icon = NULL, px = NULL, source = NULL, single = TRUE, read = TRUE) {
+  icon_name <- icon
+  icon <- map_icons
+
+  if (!is.null(icon_name)) {
+    icon <- dplyr::filter(icon, .data$name %in% icon_name)
+  }
+
+  if (!is.null(px)) {
+    icon <- dplyr::filter(icon, .data$size %in% px)
+  }
+
+  if (!is.null(source)) {
+    icon <- dplyr::filter(icon, grepl(source, repo))
+  }
+
+  if ((nrow(icon) > 1) && single) {
+    cli_abort(
+      c("{.arg icon} matches {nrow(icon)} icon, not 1.",
+        "i" = "Provide the {.arg px} and/or {.arg source} to select a single icon."
+      )
+    )
+  }
+
+  if (!read) {
+    return(icon)
+  }
+
+  dplyr::bind_cols(
+    icon,
+    "svg" = purrr::map_chr(
+      icon$url,
+      ~ paste(suppressWarnings(readLines(.x)), collapse = "\n")
+    )
+  )
+}
+
+#' Join map icons to a simple feature object with a column with the same name as
+#' iconname_col
+#'
+#' @param fun.geometry sf function to apply before returning data; defaults to
+#'   `function(x) sf::st_centroid(sf::st_zm(x))`
+#' @noRd
+#' @importFrom sf st_centroid st_zm
+#' @importFrom sfext check_sf st_transform_ext
+#' @importFrom rlang has_name
+#' @importFrom dplyr bind_cols left_join
+join_map_icons <- function(data = NULL,
+                           iconname_col = "icon",
+                           source = NULL,
+                           px = NULL,
+                           crs = NULL,
+                           fun.geometry = function(x) sf::st_centroid(sf::st_zm(x))) {
+  if (is.null(data)) {
+    return(
+      function(x) {
+        join_map_icons(
+          data = x,
+          iconname_col = iconname_col,
+          px = px,
+          source = source,
+          single = FALSE,
+          crs = crs
+        )
+      }
+    )
+  }
+
+  sfext::check_sf(data)
+
+  if (!rlang::has_name(data, iconname_col)) {
+    cli::cli_abort(
+      "{.arg iconname_col} {.value {iconname_col}} can't be found in {.arg data}."
+    )
+  }
+
+  col_icons <- sort(unique(data[[iconname_col]]))
+
+  icons <-
+    get_map_icon(
+      icon = col_icons,
+      source = source,
+      px = px,
+      single = FALSE
+    )
+
+  icons <-
+    dplyr::bind_cols(
+      icons,
+      {{ iconname_col }} := col_icons
+    )
+
+  data <-
+    dplyr::left_join(
+      data,
+      icons,
+      by = {{ iconname_col }}
+    )
+
+  data <- sfext::st_transform_ext(data, crs = crs)
+
+  suppressWarnings(fun.geometry(data))
+}
