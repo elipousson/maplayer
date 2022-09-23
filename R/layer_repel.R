feae326719bc05960a2b24f54dc2ed52aa74a5e6
Eli Pousson <eli.pousson@gmail.com>
2022-09-23 16:12:31 -0400

    feat: add location_lims param to layer_repel
    - feat: add location_lims param to layer_repel
    - fix: pass label_col param from layer_repel to geom_sf_label_repel and geom_sf_text_repel
    - refactor: add guides and margin to ggplot2 reexports

--- R/layer_repel.R
+++ R/layer_repel.R
@@ -8,6 +8,9 @@
 #'   label is provided to mapping; required otherwise.
 #' @param geom Character vector with geom to use, "text" for
 #'   [ggrepel::geom_text_repel()] or "label" for [ggrepel::geom_label_repel()].
+#' @param location_lims A `sf`, `sfc`, or `bbox` object to use in setting xlim
+#'   and ylim values if no xlim and ylim value area provided. Using this
+#'   parameter constrains labels to the bounding box of `location_lims`.
 #' @inheritParams ggrepel::geom_label_repel
 #' @inheritDotParams ggrepel::geom_label_repel -stat
 #' @export
@@ -16,6 +19,9 @@ layer_repel <- function(mapping = aes(),
                         data = NULL,
                         label_col = "name",
                         geom = c("text", "label"),
+                        location_lims = NULL,
+                        xlim = c(NA, NA),
+                        ylim = c(NA, NA),
                         ...) {
   if (!rlang::is_character(label_col, 1)) {
     cli_abort(
@@ -23,21 +29,45 @@ layer_repel <- function(mapping = aes(),
     )
   }
 
+  if (!is.null(location_lims) && all(is.na(c(ylim, xlim)))) {
+    bbox <- sfext::as_bbox(location_lims)
+
+    min <- sfext::sf_bbox_point(bbox, c("xmin", "ymin"))
+    max <- sfext::sf_bbox_point(bbox, c("xmax", "ymax"))
+
+    xlim <- c(min[[1]], max[[1]])
+    ylim <- c(min[[2]], max[[2]])
+  }
+
   geom <- rlang::arg_match(geom)
 
   switch(geom,
-    "text" = geom_sf_text_repel(mapping = mapping, data = data, ...),
-    "label" = geom_sf_label_repel(mapping = mapping, data = data, ...)
+    "text" = geom_sf_text_repel(
+      mapping = mapping,
+      data = data,
+      label_col = label_col,
+      xlim = xlim,
+      ylim = ylim,
+      ...
+    ),
+    "label" = geom_sf_label_repel(
+      mapping = mapping,
+      data = data,
+      label_col = label_col,
+      xlim = xlim,
+      ylim = ylim,
+      ...
+    )
   )
 }
 
 #' @name geom_sf_label_repel
 #' @rdname layer_repel
-geom_sf_label_repel <- function(mapping = aes(), data = NULL, ...) {
+geom_sf_label_repel <- function(mapping = aes(), data = NULL, label_col = "name", ...) {
   is_pkg_installed("ggrepel")
 
   geom_sf_coordinates(
-    mapping = aes_label(mapping, data),
+    mapping = aes_label(mapping, data, label_col),
     data = data,
     geom = ggrepel::geom_label_repel,
     ...
@@ -46,11 +76,11 @@ geom_sf_label_repel <- function(mapping = aes(), data = NULL, ...) {
 
 #' @name geom_sf_text_repel
 #' @rdname layer_repel
-geom_sf_text_repel <- function(mapping = aes(), data = NULL, ...) {
+geom_sf_text_repel <- function(mapping = aes(), data = NULL, label_col = "name", ...) {
   is_pkg_installed("ggrepel")
 
   geom_sf_coordinates(
-    mapping = aes_label(mapping, data),
+    mapping = aes_label(mapping, data, label_col),
     data = data,
     geom = ggrepel::geom_label_repel,
     ...
--- R/reexport.R
+++ R/reexport.R
@@ -9,3 +9,11 @@ ggplot2::aes
 #' @importFrom ggplot2 last_plot
 #' @export
 ggplot2::last_plot
+
+#' @importFrom ggplot2 margin
+#' @export
+ggplot2::margin
+
+#' @importFrom ggplot2 guides
+#' @export
+ggplot2::guides
