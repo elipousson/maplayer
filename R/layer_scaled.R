b9e68af83b8d7ff970ef74fee655470d83e03af1
Eli Pousson <eli.pousson@gmail.com>
2023-01-18 13:29:39 -0500

    feat: pick first scale if multiple scales match
    - feat: pick first scale if multiple scales match
    - refactor: add new bbox_fit_check helper

--- R/layer_scaled.R
+++ R/layer_scaled.R
@@ -31,19 +31,23 @@ layer_scaled <-
         scale = scale
       )
 
+    data_units <- get_dist_units(data)
+
     if (nrow(scaled_paper) > 1) {
-      cli_abort(c(
-        "{.arg paper}, {.arg orientation}, and {.arg scale} parameters returned multiple options.",
-        "More specific parameters are required."
+      cli::cli_warn(c(
+        "{.arg paper}, {.arg orientation}, and {.arg scale} parameters
+        returned multiple options.", "Using first returned option."
       ))
+
+      scaled_paper <- scaled_paper[1, ]
     }
 
-    if (is.null(asp)) {
-      asp <- scaled_paper$asp
-    }
+    asp <- asp %||% scaled_paper$asp
 
     if (!is_sf(data, ext = TRUE)) {
-      cli_abort("data must be a bounding box or simple feature object.")
+      cli::cli_abort(
+        "{.arg data} must be a {.cls bbox}, {.cls sf}, or {.cls sfc} object."
+        )
     }
 
     # Get adjusted bounding box for data
@@ -57,18 +61,14 @@ layer_scaled <-
         crs = crs
       )
 
-    # Compare bbox xdist and ydist to actual dimensions
-    # FIXME: move this into a helper function
-    ydist <- sfext::sf_bbox_ydist(bbox, units = TRUE)
-    xdist <- sfext::sf_bbox_xdist(bbox, units = TRUE)
+    if (!sfext::is_dist_units(dist)) {
+      dist <- sfext::as_dist_units(dist = dist, to = data)
+    }
 
-    check_xdist <- (xdist <= scaled_paper$width_actual)
-    check_ydist <- (ydist <= scaled_paper$height_actual)
-
-    fit_check <- (check_ydist && check_xdist)
-
-    if (!clip && !fit_check) {
-      cli_abort("This data covers a larger area than can be displayed at this scale ({scale}) on this paper ({paper})")
+    if (!clip && !bbox_fit_check(bbox, scaled_paper)) {
+      cli_abort(
+        "This data covers a larger area than can be displayed at this scale
+        ({scale}) on this paper ({paper}).")
     }
 
     scaled_dist <- max(c(scaled_paper$width_actual, scaled_paper$height_actual)) / 2
@@ -92,3 +92,26 @@ layer_scaled <-
 
     return(scaled_layer)
   }
+
+
+
+# FIXME: Replace with new sf_bbox_fit (?)
+
+#' @noRd
+#' @importFrom sfext sf_bbox_ydist sf_bbox_xdist
+bbox_fit_check <- function(bbox, paper = NULL, cols = c("actual_width", "actual_height")) {
+  # Compare bbox xdist and ydist to actual dimensions
+  # FIXME: move this into a helper function
+
+  units <- sfext::get_dist_units(paper[[cols[1]]])
+
+  ydist <- sfext::sf_bbox_ydist(bbox, units = units, drop = FALSE)
+  xdist <- sfext::sf_bbox_xdist(bbox, units = units, drop = FALSE)
+
+  print(xdist)
+  print(ydist)
+  print(paper[[cols[1]]])
+  print(paper[[cols[2]]])
+
+  (xdist <= paper[[cols[1]]]) && (ydist <= paper[[cols[2]]])
+}
