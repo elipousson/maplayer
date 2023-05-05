c6c2001d43d19ad2001d46a57720dc7839349abc
Eli Pousson <eli.pousson@gmail.com>
2023-05-05 12:23:19 -0400

    docs: tweak example for layer_neatline

--- examples/layer_neatline.R
+++ examples/layer_neatline.R
@@ -1,9 +1,11 @@
-nc <- sfext::read_sf_path(system.file("shape/nc.shp", package = "sf"))
+library(ggplot2)
 
-ggplot2::ggplot() +
+nc <- sf::read_sf(system.file("shape/nc.shp", package = "sf"))
+
+ggplot() +
   layer_location_data(data = nc) +
   layer_neatline(data = nc[1, ], asp = 1, color = "red", linewidth = 1, linetype = "dashed")
 
-ggplot2::ggplot() +
+ggplot() +
   layer_location_data(data = nc) +
   layer_neatline(data = nc[1, ], dist = 20, unit = "mi", color = "none")
