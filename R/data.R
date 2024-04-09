06a047f1b3c1cd836a5ae7476c127108b46fc7a1
your name <eli.pousson@gmail.com>
2024-04-08 22:37:47 -0400

    Update `map_icons`

--- R/data.R
+++ R/data.R
@@ -22,14 +22,14 @@
 #' Symbol Library is created by the National Park Service so is assumed to be a
 #' [Public Domain work in the U.S.](http://www.usa.gov/publicdomain/label/1.0/).
 #'
-#' This index was last updated on 2024-02-21 and may not include any icons
+#' This index was last updated on 2024-04-08 and may not include any icons
 #' that have been added since that date.
 #'
 #' @details
 #' The name column is not unique so a px or source may be required when using
 #' [layer_icon()].
 #'
-#' @format A tibble with 5,033 rows and 5 variables:
+#' @format A tibble with 5,036 rows and 5 variables:
 #' \describe{
 #'   \item{`name`}{Icon name}
 #'   \item{`url`}{Icon URL on GitHub repo}
